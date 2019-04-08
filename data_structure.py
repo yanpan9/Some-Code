990433060a44b6181eb11d82f355fa79cb9d2cc6
yanpan9 <1335624722a@gmail.com>
2019-04-08 16:39:10 +0800

    Add TreeNode structure

--- data_structure.py
+++ data_structure.py
@@ -3,3 +3,10 @@ class ListNode:
     def __init__(self, x):
         self.val = x
         self.next = None
+
+# Definition for a binary tree node.
+class TreeNode:
+    def __init__(self, x):
+        self.val = x
+        self.left = None
+        self.right = None
