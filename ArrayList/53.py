1427919c092747c41cfe2e64ce0bc79ba16a1b5e
yanpan9 <1335624722a@gmail.com>
2019-04-10 11:05:47 +0800

    max sub array

---
+++ ArrayList/53.py
@@ -0,0 +1,7 @@
+class Solution:
+    def maxSubArray(self, nums: List[int]) -> int:
+        max_ending_here, max_so_far = nums[0], nums[0]
+        for num in nums[1:]:
+            max_ending_here = max(num, max_ending_here+num)
+            max_so_far = max(max_so_far, max_ending_here)
+        return max_so_far
