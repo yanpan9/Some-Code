4e328661d726d7c2c942a16a3d5d0021b2fb32ea
yanpan9 <1335624722a@gmail.com>
2019-04-11 15:14:17 +0800

    permute unique

---
+++ ArrayList/47.py
@@ -0,0 +1,15 @@
+class Solution:
+    def permuteUnique(self, nums: List[int]) -> List[List[int]]:
+        def permutation(nums):
+            if len(nums)==1:
+                yield nums
+            else:
+                choosed = set()
+                for idx in range(len(nums)):
+                    ele = nums[idx]
+                    if ele not in choosed:
+                        choosed.add(ele)
+                        ele = [ele]
+                        for res in permutation(nums[:idx]+nums[idx+1:]):
+                            yield ele+res
+        return list(permutation(nums))
