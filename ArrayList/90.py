81e5610296f7bf238b6041686d8573e80979187f
yanpan9 <1335624722a@gmail.com>
2019-04-22 20:43:48 +0800

    subsets with duplicate

---
+++ ArrayList/90.py
@@ -0,0 +1,14 @@
+class Solution:
+    def subsetsWithDup(self, nums: List[int]) -> List[List[int]]:
+        nums.sort()
+        res, length = list(), len(nums)
+        def subsets_dfs(lst, nums, pos):
+            num = None
+            res.append(lst[:])
+            for i in range(pos, length):
+                if nums[i]!=num:
+                    lst.append(nums[i])
+                    subsets_dfs(lst, nums, i+1)
+                    num = lst.pop()
+        subsets_dfs([], nums, 0)
+        return res
