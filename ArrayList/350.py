cba45d63e85e4f27a0cf391edfc92f1378abd5df
yanpan <cnyanpan@gmail.com>
2019-04-25 16:07:04 +0800

    The intersection of two arrays 2 another solution

--- ArrayList/350.py
+++ ArrayList/350.py
@@ -1,4 +1,4 @@
-class Solution:
+class Solution_1:
     def intersect(self, nums1: List[int], nums2: List[int]) -> List[int]:
         res = list()
         if len(nums1)>len(nums2):
@@ -15,4 +15,22 @@ class Solution:
             if num in count_dict and count_dict[num]>0:
                 res.append(num)
                 count_dict[num]-=1
+        return res
+
+class Solution_2:
+    def intersect(self, nums1: List[int], nums2: List[int]) -> List[int]:
+        nums1.sort()
+        nums2.sort()
+        res = list()
+        length1, length2 = len(nums1), len(nums2)
+        i, j = 0, 0
+        while i<length1 and j<length2:
+            if nums1[i]>nums2[j]:
+                j+=1
+            elif nums1[i]<nums2[j]:
+                i+=1
+            else:
+                res.append(nums1[i])
+                i+=1
+                j+=1
         return res
