0bbbe3083392b93580e23424a26703c1ea01bfec
yanpan <cnyanpan@gmail.com>
2019-04-25 17:56:27 +0800

    update permutation

--- ArrayList/46.py
+++ ArrayList/46.py
@@ -9,3 +9,17 @@ class Solution:
                     for res in permutation(nums[:idx]+nums[idx+1:]):
                         yield ele+res
         return list(permutation(nums))
+
+class Solution_Backtracking:
+    def permute(self, nums: List[int]) -> List[List[int]]:
+        result = list()
+        def dfs(lst, nums):
+            if not nums:
+                result.append(lst[:])
+            else:
+                for i,v in enumerate(nums):
+                    lst.append(v)
+                    dfs(lst, nums[:i]+nums[i+1:])
+                    lst.pop()
+        dfs(list(), nums)
+        return result
