684ca9f0e80f6ed890888675694a152d08a0dbb8
yanpan <cnyanpan@gmail.com>
2019-05-07 16:15:22 +0800

    22 generate parentheses

---
+++ String/22.py
@@ -0,0 +1,19 @@
+class Solution:
+    def generateParenthesis(self, n: int) -> List[str]:
+        res = list()
+        def dfs(lst, n1, n2, n):
+            if n2==n:
+                res.append("".join(lst))
+                #print(res[-1])
+            else:
+                if n1<n:
+                    lst.append("(")
+                    dfs(lst, n1+1, n2, n)
+                    lst.pop()
+                if n1>n2:
+                    lst.append(")")
+                    dfs(lst, n1, n2+1, n)
+                    lst.pop()
+
+        dfs(["("], 1, 0, n)
+        return res
