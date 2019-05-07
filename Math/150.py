0ce490f585462468e3e1ac141f9cc68d69b74277
yanpan <yanpan@yanpan.local>
2019-05-07 15:39:29 +0800

    evaluate Reverse Polish Notation

---
+++ Math/150.py
@@ -0,0 +1,17 @@
+import operator
+class Solution:
+    def evalRPN(self, tokens: List[str]) -> int:
+        oper = {"+":operator.add,
+                "-":operator.sub,
+                "*":operator.mul,
+                "/":lambda a,b:int(operator.truediv(a,b))}
+        stack = list()
+        for ele in tokens:
+            if ele not in oper:
+                stack.append(int(ele))
+            else:
+                b = stack.pop()
+                a = stack.pop()
+                res = oper[ele](a, b)
+                stack.append(res)
+        return int(stack.pop())
