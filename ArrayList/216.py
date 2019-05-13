4b2023300ddd702749e68233747ae6a12961a709
yanpan9 <1335624722a@gmail.com>
2019-05-13 21:31:54 +0800

    216. Combination Sum III

---
+++ ArrayList/216.py
@@ -0,0 +1,18 @@
+class Solution:
+    def combinationSum3(self, k: int, n: int) -> List[List[int]]:
+        res = list()
+        nums = list(range(1,10))
+        def dfs(lst, k, sum_, nums):
+            if k == 0:
+                if sum_ == n:
+                    res.append(lst[:])
+                else:
+                    pass
+            elif k > 0:
+                for idx,num in enumerate(nums):
+                    if sum_ + num <= n:
+                        lst.append(num)
+                        dfs(lst, k-1, sum_ + num, nums[idx+1:])
+                        lst.pop()
+        dfs([], k, 0, nums)
+        return res
