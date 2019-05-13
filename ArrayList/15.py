e77d174744bf1fa56b672d7d7eb32176849d5b93
yanpan9 <1335624722a@gmail.com>
2019-05-13 11:31:07 +0800

    15. 3 Sum

---
+++ ArrayList/15.py
@@ -0,0 +1,25 @@
+class Solution:
+    def threeSum(self, nums: List[int]) -> List[List[int]]:
+        nums.sort()
+        res, visited = list(), set()
+        def twosum(nums, sum_):
+            hash_t = set()
+            res = list()
+            for num in nums:
+                if sum_ - num in hash_t:
+                    sub_res = [sum_-num, num]
+                    if sub_res in res:
+                        continue
+                    else:
+                        res.append(sub_res)
+                else:
+                    hash_t.add(num)
+            return res
+        for idx, num in enumerate(nums):
+            if num in visited:
+                continue
+            else:
+                visited.add(num)
+                for sub_res in twosum(nums[idx+1:], -num):
+                    res.append([num]+sub_res)
+        return res
