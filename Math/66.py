f9fc9b8da6516d479bbebd60d23ac951687430aa
yanpan <cnyanpan@gmail.com>
2019-05-28 20:10:20 +0800

    66. Plus One

---
+++ Math/66.py
@@ -0,0 +1,15 @@
+class Solution:
+    def plusOne(self, digits: List[int]) -> List[int]:
+        length, count = len(digits), 0
+        digits[-1] += 1
+        for i in range(length-1, -1, -1):
+            digits[i] += count
+            if digits[i]<10:
+                break
+            else:
+                count, val = divmod(digits[i], 10)
+                digits[i] = val
+        else:
+            if count:
+                digits.insert(0,count)
+        return digits
