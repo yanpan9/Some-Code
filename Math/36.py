0f2f39f5ab49a0a6f989438e23c342779d2a69c0
yanpan <cnyanpan@gmail.com>
2019-05-29 21:40:31 +0800

    36. Valid Sudoku

--- Math/36.py
+++ Math/36.py
@@ -19,3 +19,27 @@ class Solution:
     def check9elems(self, elems: List[int]) -> bool:
         elems = [i for i in elems if i!="."]
         return len(set(elems))==len(elems)
+
+class Solution_Hash:
+    def isValidSudoku(self, board: List[List[str]]) -> bool:
+        rg, blank = range(9), "."
+        rows = [set() for i in rg]
+        cols = [set() for i in rg]
+        boxs = [[set() for i in range(3)] for j in range(3)]
+        for i in rg:
+            for j in rg:
+                if board[i][j] != blank:
+                    ele = int(board[i][j])
+                    if ele in rows[i]:
+                        return False
+                    else:
+                        rows[i].add(ele)
+                    if ele in cols[j]:
+                        return False
+                    else:
+                        cols[j].add(ele)
+                    if ele in boxs[i//3][j//3]:
+                        return False
+                    else:
+                        boxs[i//3][j//3].add(ele)
+        return True
