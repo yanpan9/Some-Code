435fa56f8f74dd975e1b1de614c3927aef6ab455
yanpan9 <1335624722a@gmail.com>
2019-06-02 13:40:26 +0800

    35. Search Insert Position

---
+++ ArrayList/35.py
@@ -0,0 +1,17 @@
+class Solution:
+    def searchInsert(self, nums: List[int], target: int) -> int:
+        length = len(nums)
+        if target<=nums[0]:
+            return 0
+        elif target>nums[-1]:
+            return length
+        else:
+            L, R = 0, length-1
+            while True:
+                mid = int((L+R)/2)
+                if nums[mid]<target:
+                    L = mid
+                else:
+                    R = mid
+                if L+1 == R:
+                    return R
