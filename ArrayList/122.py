966daee00c57850627ebb8473a683ed26ef7a9da
yanpan9 <1335624722a@gmail.com>
2019-06-14 17:00:28 +0800

    122. Best Time to Buy and Sell Stock II

---
+++ ArrayList/122.py
@@ -0,0 +1,9 @@
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int:
+        length = len(prices)
+        sum_ = 0
+        for i in range(0, length-1):
+            sub_ = prices[i+1]-prices[i]
+            if sub_>0:
+                sum_ += sub_
+        return sum_
