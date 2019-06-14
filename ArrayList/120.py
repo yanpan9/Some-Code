7767a64aa07d8cfc1f78769081251fb94201c6ba
yanpan9 <1335624722a@gmail.com>
2019-06-14 13:17:39 +0800

    120. Triangle Minimum Total

---
+++ ArrayList/120.py
@@ -0,0 +1,7 @@
+class Solution:
+    def minimumTotal(self, triangle: List[List[int]]) -> int:
+        length = len(triangle)
+        for i in range(length-2, -1, -1):
+            for j in range(i+1):
+                triangle[i][j] += min(triangle[i+1][j], triangle[i+1][j+1])
+        return triangle[0][0]
