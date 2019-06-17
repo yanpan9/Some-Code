39b35ebbfd29be9fc170ecb1b2ba83bbba04b48d
yanpan9 <1335624722a@gmail.com>
2019-06-17 22:06:06 +0800

    144. Binary Tree Preorder Traversal

---
+++ TreeNode/144.py
@@ -0,0 +1,31 @@
+from data_structure import TreeNode
+
+class Solution_Recur:
+    def preorderTraversal(self, root: TreeNode) -> List[int]:
+        lst = list()
+        if root:
+            self._preorderTraversal(root, lst)
+            return lst
+        else:
+            return lst
+            
+    def _preorderTraversal(self, root: TreeNode, lst: List):
+        lst.append(root.val)
+        for c in (root.left, root.right):
+            if c:
+                self._preorderTraversal(c, lst)
+
+class Solution_Stack:
+    def preorderTraversal(self, root: TreeNode) -> List[int]:
+        res = list()
+        if root:
+            lst = [root,]
+            while lst:
+                node = lst.pop()
+                res.append(node.val)
+                for c in (node.right, node.left):
+                    if c:
+                        lst.append(c)
+            return res
+        else:
+            return res
