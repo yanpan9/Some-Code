218ff019064c519d0dc79fe1540ccc7b825ec9df
yanpan9 <1335624722a@gmail.com>
2019-06-19 16:25:04 +0800

    429. N-ary Tree Level Order Traversal

---
+++ TreeNode/429.py
@@ -0,0 +1,16 @@
+from data_structure import Node
+from collections import deque
+
+class Solution:
+    def levelOrder(self, root: 'Node') -> List[List[int]]:
+        res = list()
+        if root:
+            queue = deque([root])
+            while queue:
+                sub_res = list()
+                for _ in range(len(queue)):
+                    node = queue.popleft()
+                    sub_res.append(node.val)
+                    queue.extend(node.children)
+                res.append(sub_res)
+        return res
