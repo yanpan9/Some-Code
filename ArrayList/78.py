cdbb192052d1a59417663ae7342cf3a4b963b5e8
yanpan9 <1335624722a@gmail.com>
2019-06-20 16:00:50 +0800

    78. Subsets

---
+++ ArrayList/78.py
@@ -0,0 +1,11 @@
+class Solution:
+    def subsets(self, nums: List[int]) -> List[List[int]]:
+        return list(self._subsets([], nums))
+    
+    def _subsets(self, lst: List[int], nums: List[int]) -> List[int]:
+            yield lst[:]
+            for i,v in enumerate(nums):
+                lst.append(v)
+                for sub_res in self._subsets(lst, nums[i+1:]):
+                    yield sub_res
+                lst.pop()
