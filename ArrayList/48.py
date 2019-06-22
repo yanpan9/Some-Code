3ede6d359152425f2d3a0550b83f4e3462fd6760
yanpan9 <1335624722a@gmail.com>
2019-06-22 12:36:55 +0800

    48. Rotate image

---
+++ ArrayList/48.py
@@ -0,0 +1,16 @@
+class Solution:
+    def rotate(self, matrix: List[List[int]]) -> None:
+        """
+        Do not return anything, modify matrix in-place instead.
+        """
+        n = len(matrix)
+        # Traverse row
+        for i in range(n//2):
+            # Traverse element which need to be rotate
+            # in the row
+            for j in range(i, n-1-i):
+                pixel = (i,j)
+                cache = matrix[i][j]
+                for _ in range(4):
+                    i, j = j, n-1-i
+                    cache, matrix[i][j] = matrix[i][j], cache
