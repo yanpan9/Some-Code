7f46696e305e065b211e6dad8f1e9ec5cb23c4b0
yanpan <cnyanpan@gmail.com>
2019-07-02 15:10:45 +0800

    49. Update Func

--- String/49.py
+++ String/49.py
@@ -4,13 +4,13 @@ class Solution:
     def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
         res = dict()
         for s in strs:
-            number = self.str2str(s)
+            number = self.str2int(s)
             if number in res:
                 res[number].append(s)
             else:
                 res[number] = [s]
         return list(res.values())
-    def str2str(self, s: str) -> str:
+    def str2int(self, s: str) -> int:
         number, step = 0, 3
         for char in s:
             number += 1<<((ord(char)-97)*step)
