79904ea3db6f2afb6fc66195a1558ce1793a0b3b
yanpan9 <1335624722a@gmail.com>
2019-07-27 16:24:52 +0800

    57. Use hash table

--- String/57.py
+++ String/57.py
@@ -1,6 +1,6 @@
 from typing import List
 
-class Solution_BruteForce:
+class Solution:
     def findSubstring(self, s: str, words: List[str]) -> List[int]:
         res = list()
         m = len(words)
@@ -8,13 +8,18 @@ class Solution_BruteForce:
             n = len(words[0])
             length = len(s)
             idx, sub_len = 0, m*n-1
+            counter = dict()
+            for word in words:
+                counter[word]=counter.get(word, 0)+1
             while idx<length-sub_len:
                 if s[idx:idx+n] in words:
-                    t_words = words[:]
+                    t_words = counter.copy()
                     for i in range(m):
                         sub_str = s[(idx+i*n):(idx+(i+1)*n)]
                         if sub_str in t_words:
-                            t_words.remove(sub_str)
+                            t_words[sub_str]-=1
+                            if not t_words[sub_str]:
+                                t_words.pop(sub_str)
                         else:
                             break
                     else:
