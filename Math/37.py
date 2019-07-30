a88f7cec756c53cea8ae049ce67894d8ed44aa14
yanpan <cnyanpan@gmail.com>
2019-07-30 18:31:13 +0800

    37. Update code

--- Math/37.py
+++ Math/37.py
@@ -5,43 +5,35 @@ class Solution:
         """
         Do not return anything, modify board in-place instead.
         """
-        def dfs(i, j):
-            if board[i][j]!=blank:
-                if i==8 and j==8:
-                    return True
-                else:
-                    idx = i*9+j+1
-                    new_i, new_j = divmod(idx, 9)
-                    return dfs(new_i, new_j)
+        def dfs(idx):
+            if idx == 9*9:
+                return True
             else:
-                for num in rg:
-                    num += 1
-                    if (num not in rows[i] and
-                        num not in cols[j] and
-                        num not in boxs[i//3][j//3]):
-                        board[i][j]=str(num)
-                        rows[i].add(num)
-                        cols[j].add(num)
-                        boxs[i//3][j//3].add(num)
-                        if i==8 and j==8:
-                            return True
-                        else:
-                            idx = i*9+j+1
-                            new_i, new_j = divmod(idx, 9)
-                            flag = dfs(new_i, new_j)
-                            if not flag:
-                                board[i][j] = blank
-                                rows[i].remove(num)
-                                cols[j].remove(num)
-                                boxs[i//3][j//3].remove(num)
-                            else:
+                i, j = divmod(idx, 9)
+                if board[i][j]==blank:
+                    for num in rg:
+                        num += 1
+                        if (num not in rows[i] and
+                            num not in cols[j] and
+                            num not in boxs[i//3][j//3]):
+                            board[i][j]=str(num)
+                            rows[i].add(num)
+                            cols[j].add(num)
+                            boxs[i//3][j//3].add(num)
+                            if dfs(idx+1):
                                 return True
-                    
+                            board[i][j] = blank
+                            rows[i].remove(num)
+                            cols[j].remove(num)
+                            boxs[i//3][j//3].remove(num)
+                else:
+                    if dfs(idx+1):
+                        return True
                 
         rg,blank = range(9), "."
         rows = [set() for _ in rg]
         cols = [set() for _ in rg]
-        boxs = [[set() for _ in range(3)] for j in range(3)]
+        boxs = [[set() for _ in range(3)] for _ in range(3)]
         for i in rg:
             for j in rg:
                 if board[i][j]!=blank:
@@ -49,4 +41,4 @@ class Solution:
                     rows[i].add(ele)
                     cols[j].add(ele)
                     boxs[i//3][j//3].add(ele)
-        dfs(0,0)
+        dfs(0)
