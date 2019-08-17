177ff05f6d4819f5a61aa423d661bf4bd495dd7b
yanpan9 <1335624722a@gmail.com>
2019-08-17 13:51:14 +0800

    347. Top k frequent elements

---
+++ ArrayList/347.py
@@ -0,0 +1,54 @@
+from collections import Counter
+from typing import List
+
+class Solution_MaxHeap:
+    def topKFrequent(self, nums: List[int], k: int) -> List[int]:
+        counter = Counter(nums)
+        array = list(counter.items())
+        length = len(array)
+        res = list()
+        for i in range((length-1)//2, -1, -1):
+            self.adjustHeap(array, i, length, key=lambda x:x[1])
+        for _ in range(k):
+            res.append(array[0][0])
+            length -= 1
+            array[0] = array[length]
+            self.adjustHeap(array, 0, length, key=lambda x:x[1])
+        return res
+            
+    def adjustHeap(self, heap, i, length, key=lambda x:x):
+        val = heap[i]
+        while 2*i+1<length:
+            idx = 2*i+1
+            if idx+1<length and key(heap[idx+1])>key(heap[idx]):
+                idx += 1
+            if key(heap[idx])>key(val):
+                heap[i] = heap[idx]
+                i = idx
+            else:
+                break
+        heap[i] = val
+
+class Solution_MinHeap:
+    def topKFrequent(self, nums: List[int], k: int) -> List[int]:
+        counter = Counter(nums)
+        array = [(0,0) for _ in range(k)]
+        for key in counter:
+            if counter[key]>array[0][1]:
+                array[0] = (key, counter[key])
+                self.adjustHeap(array, 0, k, key=lambda x:x[1])
+        res = sorted(array, key=lambda x:x[1], reverse=True)
+        return [i[0] for i in res]
+            
+    def adjustHeap(self, heap, i, length, key=lambda x:x):
+        val = heap[i]
+        while 2*i+1<length:
+            idx = 2*i+1
+            if idx+1<length and key(heap[idx+1])<key(heap[idx]):
+                idx += 1
+            if key(heap[idx])<key(val):
+                heap[i] = heap[idx]
+                i = idx
+            else:
+                break
+        heap[i] = val
