c9d6fffa34006e9c3327432f5f3039ed108ec724
yanpan <cnyanpan@gmail.com>
2019-09-29 16:26:59 +0800

    235. update iteration solution

--- TreeNode/235.py
+++ TreeNode/235.py
@@ -1,6 +1,6 @@
 from data_structure import TreeNode
 
-class Solution:
+class Solution_Recur:
     def lowestCommonAncestor(self, root: 'TreeNode', p: 'TreeNode', q: 'TreeNode') -> 'TreeNode':
         if root:
             return self.findAncestor(root, p.val, q.val)
@@ -15,3 +15,17 @@ class Solution:
             return self.findAncestor(root.right, p, q)
         else:
             return root
+
+class Solution_Iter:
+    def lowestCommonAncestor(self, root: 'TreeNode', p: 'TreeNode', q: 'TreeNode') -> 'TreeNode':
+        if root:
+            p, q = p.val, q.val
+            while True:
+                if p<root.val and q<root.val:
+                    root = root.left
+                elif p>root.val and q>root.val:
+                    root = root.right
+                else:
+                    return root
+        else:
+            return None
